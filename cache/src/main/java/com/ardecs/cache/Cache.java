9caed0ff900cab94b3839b22301582c32bf8e8d5
Nazarov Ivan <79447737+Nazarov-Ivan@users.noreply.github.com>
2022-09-02 14:50:41 +0400

    Delete cache directory

--- cache/src/main/java/com/ardecs/cache/Cache.java
+++
@@ -1,9 +0,0 @@
-package com.ardecs.cache;
-
-public interface Cache {
-    void add(String key, String value);
-
-    Object get(String key);
-
-    void allDelete();
-}
--- cache/src/main/java/com/ardecs/cache/Strategy.java
+++
@@ -1,9 +0,0 @@
-package com.ardecs.cache;
-
-public interface Strategy {
-    void add(String key, String value);
-
-    Object get(String key);
-
-    void clear();
-}
