a7dd87869646d5b57a73947b3836c9a0c3c320a6
mrvanka <mr.vanka2014@mail.ru>
2022-09-02 14:46:58 +0400

    Cache in new directory

---
+++ cache/src/main/java/com/ardecs/cache/LRU.java
@@ -0,0 +1,18 @@
+package com.ardecs.cache;
+
+public class LRU implements Strategy{
+    @Override
+    public void add(String key, String value) {
+
+    }
+
+    @Override
+    public Object get(String key) {
+        return null;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+}
