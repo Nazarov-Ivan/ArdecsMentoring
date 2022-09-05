a3aed341fd4ec0a1d9830c5d600f239776465fe9
mrvanka <mr.vanka2014@mail.ru>
2022-09-05 05:23:57 +0400

    Add model

---
+++ Cache/src/main/java/com/ardecs/cache/models/User.java
@@ -0,0 +1,29 @@
+package com.ardecs.cache.models;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+    private Integer id;
+    private String name;
+
+    public User(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
