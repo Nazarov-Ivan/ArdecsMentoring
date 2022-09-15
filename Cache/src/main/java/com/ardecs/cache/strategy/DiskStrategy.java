f9d8bdd5d8a53c5790660c57821a29b7109f7aa8
mrvanka <mr.vanka2014@mail.ru>
2022-09-16 02:20:31 +0400

    Изменения в связи с коррективами

--- Cache/src/main/java/com/ardecs/cache/strategy/DiskStrategy.java
+++ Cache/src/main/java/com/ardecs/cache/strategy/DiskStrategy.java
@@ -1,7 +1,40 @@
 package com.ardecs.cache.strategy;
 
-public interface DiskStrategy<K, V> extends Strategy<K, V> {
-    void downloadToDisk();
+import com.sun.org.slf4j.internal.Logger;
+import com.sun.org.slf4j.internal.LoggerFactory;
 
-    void uploadFromDisk();
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
+
+
+public abstract class DiskStrategy{
+    private final static Logger LOGGER = LoggerFactory.getLogger(DiskStrategy.class);
+
+    public static void downloadToDisk(HashMap mapCache, String fileName) {
+        try (ObjectOutputStream objectOutputStream = new ObjectOutputStream(Files.newOutputStream(Paths.get(fileName)));) {
+            objectOutputStream.writeObject(mapCache);
+        } catch (IOException e) {
+            LOGGER.error("Cache didn't save to disk", e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void uploadFromDisk(HashMap mapCache, String fileName) {
+        try (ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream(fileName))) {
+            mapCache = (HashMap) objectInputStream.readObject();
+        } catch (FileNotFoundException | ClassNotFoundException ex) {
+            LOGGER.error("File or class not found", ex);
+            throw new RuntimeException(ex);
+        } catch (IOException ex) {
+            LOGGER.error("Disk is empty", ex);
+        }
+    }
+
 }
--- Cache/src/main/java/com/ardecs/cache/strategy/LFU.java
+++ Cache/src/main/java/com/ardecs/cache/strategy/LFU.java
@@ -1,36 +1,48 @@
 package com.ardecs.cache.strategy;
 
-import com.ardecs.cache.cache.KeyNotFoundException;
+import com.ardecs.cache.exceptions.KeyNotFoundException;
 
+import java.io.FileNotFoundException;
 import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
 
-
-public class LFU<K, V extends Serializable> implements Strategy<K, V> {
+public class LFU<K, V extends Serializable> implements Strategy<K, V>, Serializable {
     private final int sizeOfCache;
-    protected HashMap<K, V> mapCache;
-    protected HashMap<K, Integer> mapCountOfUsing;
+
+    protected HashMap<K, CacheEntry> mapCache;
+
+    protected class CacheEntry implements Comparable<CacheEntry>, Serializable {
+        Integer countOfUse;
+        V value;
+
+        CacheEntry(V value) {
+            countOfUse = 1;
+            this.value = value;
+        }
+
+        @Override
+        public int compareTo(CacheEntry o) {
+            return this.countOfUse.compareTo(o.countOfUse);
+        }
+    }
 
     public LFU(int sizeOfCache) {
         this.sizeOfCache = sizeOfCache;
         mapCache = new HashMap<>(sizeOfCache);
-        mapCountOfUsing = new HashMap<>(sizeOfCache);
     }
 
-    public void add(K key, V value) {
+    protected void add(K key, V value) {
         while (mapCache.size() >= this.sizeOfCache) {
-            K minKey = mapCountOfUsing.entrySet().stream()
-                    .min(Map.Entry.comparingByValue(Integer::compareTo)).get().getKey();
-            mapCountOfUsing.remove(minKey);
+            K minKey = mapCache.entrySet().stream()
+                    .min(Map.Entry.comparingByValue(CacheEntry::compareTo)).orElse(null).getKey();
             mapCache.remove(minKey);
         }
-        mapCache.put(key, value);
-        mapCountOfUsing.put(key, 1);
+        mapCache.put(key, new CacheEntry(value));
     }
 
     @Override
-    public void put(K key, V value) {
+    public void put(K key, V value)  {
         add(key, value);
     }
 
@@ -39,16 +51,13 @@ public class LFU<K, V extends Serializable> implements Strategy<K, V> {
         if (mapCache.size() == 0) {
             throw new KeyNotFoundException("key " + key + " not found in cache");
         } else if (mapCache.containsKey(key)) {
-            int countOfUse = mapCountOfUsing.get(key);
-            countOfUse++;
-            mapCountOfUsing.put(key, countOfUse);
-            return mapCache.get(key);
+            mapCache.get(key).countOfUse++;
+            return mapCache.get(key).value;
         } else throw new KeyNotFoundException("key " + key + " not found in cache");
     }
 
     @Override
     public void clear() {
         mapCache.clear();
-        mapCountOfUsing.clear();
     }
 }
